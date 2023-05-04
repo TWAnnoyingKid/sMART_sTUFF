37ac920ac5bfdb784eccec9a693b831cf16db8d6
TWAnnoyingKid <109130275+TWAnnoyingKid@users.noreply.github.com>
2023-05-05 07:39:30 +0800

    Add files via upload

--- FDB_ver5/BME_FDB_ver5/BME_FDB_ver5.ino
+++ FDB_ver5/BME_FDB_ver5/BME_FDB_ver5.ino
@@ -7,15 +7,17 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include <Firebase_ESP_Client.h>
+#include <NTPClient.h>
 
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
 String ele = "bme";
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "time.stdtime.gov.tw");
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 Adafruit_BME280 bme;
-unsigned long delayTime;
 
 WiFiServer server(80);
 
@@ -29,7 +31,7 @@ unsigned long previousTime = 0;
 const long timeoutTime = 2000;
 #define DATABASE_URL "https://esp8266-ai2-default-rtdb.firebaseio.com"
 #define API_KEY "AIzaSyAF4OdtYUAomk_4WnvE5MXb_nphlQ33UyA" 
-FirebaseData fbdo;
+FirebaseData fbdo, fbdo_D1, fbdo_D2, fbdo_D4;
 FirebaseAuth auth;
 FirebaseConfig config;
 unsigned long sendDataPrevMillis = 0;
@@ -37,10 +39,10 @@ int count = 0;
 bool signupOK = false;
 
 String A = "";
-String T = "";
-String H = "";
+String IP = "";
 String STAT1 = "";
 String STAT2 = "";
+String CNSTAT = "";
 
 void setup() {
   Serial.begin(115200);
@@ -49,7 +51,6 @@ void setup() {
   digitalWrite(rs, HIGH);
   bool status;
   status = bme.begin(0x76); 
-  delayTime = 1000;
   
   WiFiManager wifiManager;
 
@@ -61,8 +62,13 @@ void setup() {
   wifiManager.autoConnect("SmartStuff");
 
   A = WiFi.macAddress();
+  IP =  WiFi.localIP().toString();
+  timeClient.begin();
+  timeClient.setTimeOffset(28800);
+
   STAT1 = A + "/Temperature";
   STAT2 = A + "/Humidity";
+  CNSTAT = A + "/esp";
   
   config.api_key = API_KEY;
   config.database_url = DATABASE_URL;
@@ -76,36 +82,40 @@ void setup() {
   config.token_status_callback = tokenStatusCallback;
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
-  Firebase.RTDB.setFloat(&fbdo, STAT1, bme.readTemperature());
-  Firebase.RTDB.setFloat(&fbdo, STAT2, bme.readHumidity());
+
+  if (!Firebase.RTDB.beginStream(&fbdo_D1, STAT1)) {
+    Serial.print ("D1 begin error ");
+    Serial.println(fbdo_D1.errorReason());
+  }
+  if (!Firebase.RTDB.beginStream(&fbdo_D2, STAT2)) {
+    Serial.print ("D2 begin error ");
+    Serial.println(fbdo_D2.errorReason());
+  }
+  if  (!Firebase.RTDB.beginStream(&fbdo_D4, CNSTAT)){
+    Serial.printf("CNSTAT begin error", fbdo_D4.errorReason().c_str());
+  } 
+  Firebase.RTDB.setFloat(&fbdo_D1, STAT1, bme.readTemperature());
+  Firebase.RTDB.setFloat(&fbdo_D2, STAT2, bme.readHumidity());
+  Firebase.RTDB.setString(&fbdo_D4, CNSTAT, "0");
+  
 
   server.begin();
+  Serial.println("ALL DONE");
 }
 
 void loop(){
+  Web();
+  call();
+  time();
+
+  if (Firebase.isTokenExpired()){
+    Firebase.refreshToken(&config);
+    Serial.println("Refresh token");
+  }
+
+}
+void Web(){
   WiFiClient client = server.available();
-  delay(delayTime);  
-
-
-  
-  if (Firebase.RTDB.getFloat(&fbdo, STAT1)) {
-    Serial.print("Temperature = ");
-    Serial.print(bme.readTemperature());
-    Serial.println(" *C");
-    T = fbdo.stringData() + " *C";
-  }
-  delay(10);
-  if (Firebase.RTDB.getFloat(&fbdo, STAT2)) {
-    Serial.print("Humidity = ");
-    Serial.print(bme.readHumidity());
-    Serial.println(" %");
-    H = fbdo.stringData() + " %";
-  }
-  delay(10);
-  Firebase.RTDB.setFloat(&fbdo, STAT1, bme.readTemperature());
-  Firebase.RTDB.setFloat(&fbdo, STAT2, bme.readHumidity());
-  
-
   if (client) {                           
     Serial.println("New Client.");       
     String currentLine = "";               
@@ -138,10 +148,10 @@ void loop(){
             
 
             client.println("<body><h1>MAC=" + A + "</h1>");
-            client.println("<body><h2>ELEMENT=" + ele + "</h2>");
-            client.println("<body><h3>裝置名稱 = sMART sTUFF 溫溼度感測器</h3>");
-
-            
+            client.println("<h2>ELEMENT=" + ele + "</h2>");
+            client.println("<h3>裝置名稱 = sMART sTUFF 溫溼度感測器</h3>");
+            client.println("<h4>IP=" + IP + "</h4>");
+            client.println("</body></html>");
             client.println();
             break;
           } else { 
@@ -157,4 +167,50 @@ void loop(){
     Serial.println("Client disconnected.");
     Serial.println("");
   }
+}
+void call(){
+  if(Firebase.ready() && signupOK){
+    if  (!Firebase.RTDB.readStream(&fbdo_D1)){
+       Serial.print("Temp read error：");
+       Serial.println(fbdo_D1.errorReason());
+    }  
+    if (fbdo_D1.streamAvailable()) {
+      Serial.print(fbdo_D1.floatData());
+      Serial.print(" *C ");
+    }
+  }
+  if(Firebase.ready() && signupOK){
+    if  (!Firebase.RTDB.readStream(&fbdo_D2)){
+       Serial.print("Humid read error：");
+       Serial.println(fbdo_D2.errorReason());
+    }  
+    if (fbdo_D2.streamAvailable()) {
+      Serial.print(fbdo_D2.floatData());
+      Serial.println(" % ");
+    }
+  }
+  if(Firebase.ready() && signupOK){
+    if  (!Firebase.RTDB.readStream(&fbdo_D4)){
+       Serial.print("cn read error：");
+       Serial.println(fbdo_D4.errorReason());
+    }  
+    if (fbdo_D4.streamAvailable()) {
+      if (fbdo_D4.stringData() == "1") {
+        Firebase.RTDB.setString(&fbdo_D4, CNSTAT, "2");
+      }
+    }
+  }
+}
+void time() {
+  timeClient.update();
+  if (timeClient.getMinutes() == 15 || timeClient.getMinutes() == 30 || timeClient.getMinutes() == 45 || timeClient.getMinutes() == 0){
+    if (timeClient.getSeconds() < 10 & timeClient.getSeconds() > 7) {
+      Firebase.RTDB.setString(&fbdo_D4, CNSTAT, "1");
+    }
+  }
+  if (timeClient.getSeconds() == 0 || timeClient.getSeconds() == 15 || timeClient.getSeconds() == 30 || timeClient.getSeconds() == 45) {
+    Firebase.RTDB.setFloat(&fbdo, STAT1, bme.readTemperature());
+    Firebase.RTDB.setFloat(&fbdo, STAT2, bme.readHumidity());
+  }
+  
 }
